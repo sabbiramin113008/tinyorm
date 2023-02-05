c18c7cf019c365597a187a1a279a1263973f2ad2
sabbiramin113008 <sabbiramin.cse11ruet@gmail.com>
2023-02-05 15:12:50 +0600

    bumping version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='lightorm',
-    version='0.0.2',
+    version='0.0.3',
     author='Sabbir Amin',
     author_email='sabbiramin.cse11ruet@gmail.com',
     description='Yet another, super lightweight MySQL ORM in Python.',
